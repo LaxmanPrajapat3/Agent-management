014e4a6e76c9cfdc79606a951caeb7de332907ac
Laxman <theprogrammer452023@gmail.com>
2025-10-02 10:17:25 +0530

    step up assignmentschema

---
+++ Backend/models/Assinment.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const AssignmentSchema = new mongoose.Schema({
+  agent: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent', required: true },
+  entries: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Entry' }],
+  uploadRef: { type: String } // optional
+}, { timestamps: true });
+
+module.exports = mongoose.model('Assignment', AssignmentSchema);
